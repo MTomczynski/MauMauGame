c4821be6a1863f42608d1df3c4ceb8858e2f3617
MTomczynski <mj.tomczynski@gmail.com>
2016-06-18 23:50:15 +0200

    clearing tags

--- src/maumaugame/Hand.java
+++ src/maumaugame/Hand.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package maumaugame;
 
 import java.util.ArrayList;
-import java.util.List;
 
-/**
- *
- * @author M
- */
 public class Hand
 {
 
